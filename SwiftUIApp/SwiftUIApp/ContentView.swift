e3b94deae9728a26455ba40c1dd53c85ff45d971
zhenru.wang <wzz0617@126.com>
2020-04-18 22:02:25 +0900

    SwiftUIアプリプロジェクトを追加する

---
+++ SwiftUIApp/SwiftUIApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIApp
+//
+//  Created by admin on 2020/04/18.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
