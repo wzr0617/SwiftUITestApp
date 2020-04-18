aaf86d94323a088d6abbef83af253261dc0f9554
zhenru.wang <wzz0617@126.com>
2020-04-18 21:48:40 +0900

    テストアプリのプロジェクトを作成する

---
+++ SwiftUITestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftUITestApp
+//
+//  Created by admin on 2020/04/18.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
