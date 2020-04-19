198398d027467860271d1e291e42d8d053d1cee0
zhenru.wang <wzz0617@126.com>
2020-04-20 03:51:50 +0900

    単純SwiftUIを利用して、SwiftUIのテストアプリを作成する

--- SwiftUIApp/SwiftUIApp/ContentView.swift
+++ SwiftUIApp/SwiftUIApp/ContentView.swift
@@ -9,13 +9,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    var rooms: [Room] = []
+
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List(rooms) { room in
+                RoomCell(room: room)
+            }
+            .navigationBarTitle(Text("Rooms"))
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(rooms: testData)
+    }
+}
+
+struct RoomCell: View {
+    let room: Room
+
+    var body: some View {
+        NavigationLink(destination: RoomDetail(room: room)) {
+            Image(room.thumbNailName)
+                .cornerRadius(16)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text(room.name)
+                    Text("\(room.capacity) people")
+                        .font(.subheadline).foregroundColor(.secondary)
+                }
+            }
+        }
     }
 }
